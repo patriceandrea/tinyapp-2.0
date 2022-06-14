61a3f489c9913201745be08bcdb705563823f8df
patriceandrea <andrea_pavico98@yahoo.ca>
2022-06-14 15:23:23 -0400

    add components into the register page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../Components/Header";
 import AuthenTextField from "../Components/AuthenTextField";
-import "../stylesheet/AuthenTextField.css"
+
 
 export interface ILoginProps { }
 

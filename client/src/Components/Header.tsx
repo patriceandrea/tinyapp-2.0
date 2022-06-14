8d6dc27a4cced976dbc40a36b45895694e401efc
patriceandrea <andrea_pavico98@yahoo.ca>
2022-06-14 14:22:36 -0400

    fix the header

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -22,24 +22,20 @@
 //   )
 // };
 
-// export default Header; 
+// export default Header;
 
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-// const pages = ['My Url', 'Create a new Url'];
-const sessions = ['Login', 'Register'];
 
-const ResponsiveAppBar = () => {
+
+const Header = () => {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 
@@ -70,62 +66,20 @@ const ResponsiveAppBar = () => {
             TinyApp
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
 
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
 
-              <MenuItem onClick={handleCloseNavMenu}>
-                <Typography textAlign="center">My Url</Typography>
-              </MenuItem>
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            <Typography>
 
-
-            <Link to="/myurls" >  <Button
-              onClick={handleCloseNavMenu}
-              sx={{ my: 2, color: 'white', display: 'block' }}
-            >
-              My Url
-            </Button>
-            </Link>
-
+              <Link to="/myurls" >
+                <Button
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  My Url
+                </Button>
+              </Link>
+            </Typography>
 
             <Link to="/newurls" > <Button
               onClick={handleCloseNavMenu}
@@ -167,4 +121,5 @@ const ResponsiveAppBar = () => {
 
   );
 };
-export default ResponsiveAppBar;
+export default Header;
+
